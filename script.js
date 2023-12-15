4c4372c0b51cba8e0cd3bc929eb87dad2cc9b7f9
R_Van <reijhxvan.campos@gmail.com>
2023-12-16 00:28:18 +0800

    moved dataset folder, implemented model prediction from a saved model

--- script.js
+++ script.js
@@ -1,47 +1,6 @@
-const IMG_DIR = "./assets/dataset/Images/"
-const TRASH_RED_ICON = "./assets/icons/trash_red.png"
-const TRASH_GREEN_ICON = "./assets/icons/trash_green.png"
-const ROADS = [
-    {name: "Clean Road", image: IMG_DIR + "clean_1.jpg",},
-    {name: "Clean Road", image: IMG_DIR + "clean_2.jpg",},
-    {name: "Clean Road", image: IMG_DIR + "clean_3.jpg",},
-    {name: "Clean Road", image: IMG_DIR + "clean_4.jpg",},
-    {name: "Clean Road", image: IMG_DIR + "clean_5.jpg",},
-    {name: "Dirty Road", image: IMG_DIR + "dirty_1.jpg",},
-    {name: "Dirty Road", image: IMG_DIR + "dirty_2.jpg",},
-    {name: "Dirty Road", image: IMG_DIR + "dirty_3.jpg",},
-    {name: "Dirty Road", image: IMG_DIR + "dirty_4.jpg",},
-    {name: "Dirty Road", image: IMG_DIR + "dirty_5.jpg",},
-    // Add more ROADS as needed
-];
-
-// Add click event listener to each marker
-let markers = document.querySelectorAll('.marker');
-markers.forEach((marker, index) => {
-    let road = ROADS[index];
-    let prediction = 0
-
-    // Set background image based on prediction value
-    marker.style.backgroundImage = `url(${prediction === 1 ? TRASH_RED_ICON : TRASH_GREEN_ICON})`;
-
-    marker.addEventListener('click', () => {
-        // Update sidebar content
-        document.getElementById('road-name').innerText = road.name;
-        document.getElementById('road-image').src = road.image;
-        // Show sidebar
-        document.getElementById('sidebar').style.display = 'block';
-    });
-});
-
-// Close sidebar when clicking outside of it
-document.addEventListener('click', (event) => {
-    if (!event.target.closest('#sidebar') && !event.target.classList.contains('marker')) {
-        document.getElementById('sidebar').style.display = 'none';
-    }
-});
-
-
-// ALL MODEL RELATED
+// ------------------------------
+// All Models Related
+// ------------------------------
 const MOBILE_NET_INPUT_WIDTH = 224;
 const MOBILE_NET_INPUT_HEIGHT = 224;
 let mobilenet;
@@ -58,25 +17,24 @@ async function loadMobileNetFeatureModel() {
 
     // Warm up the model by passing zeros through it once.
     tf.tidy(function () {
-        let answer = mobileNetBase.predict(tf.zeros([1, MOBILE_NET_INPUT_HEIGHT, MOBILE_NET_INPUT_WIDTH, 3]));
+        mobileNetBase.predict(tf.zeros([1, MOBILE_NET_INPUT_HEIGHT, MOBILE_NET_INPUT_WIDTH, 3]));
         console.log("Base model loaded successfully!")
     });
 }
 
-model = tf.sequential();
-model.add(tf.layers.dense({inputShape: [1280], units: 128, activation: 'relu'}));
-model.add(tf.layers.dense({units: 1, activation: 'sigmoid'}));
+function createNewModelHead(){
+    model = tf.sequential();
+    model.add(tf.layers.dense({inputShape: [1280], units: 128, activation: 'relu'}));
+    model.add(tf.layers.dense({units: 1, activation: 'sigmoid'}));
 
-// Compile the model with the defined optimizer and specify a loss function to use.
-model.compile({
-    optimizer: 'adam',
-    loss: 'binaryCrossentropy',
-    metrics: ['accuracy']
-});
-console.log("New model head loaded successfully!")
-model.summary()
+    model.compile({
+        optimizer: 'adam',
+        loss: 'binaryCrossentropy',
+        metrics: ['accuracy']
+    });
+    console.log("New model head loaded successfully!")
+}
 
-// Function to preprocess the image and get the tensor
 function preprocessImage(img) {
     const tensor = tf.tidy(() => {
         // Convert the image to a tensor
@@ -95,28 +53,35 @@ function preprocessImage(img) {
 // Function to load and preprocess an image
 async function loadAndPreprocessImage(filename) {
     const img = new Image();
-    let result;
     img.src = filename;
     await img.decode(); // Ensure the image is loaded
-    result = preprocessImage(img);
-    result = mobileNetBase.predict(result).squeeze();
-    return result
+    return preprocessImage(img); // has shape [1,224,224,3]
 }
-async function prepareDataset(){
-    const metadata = await fetch('./assets/dataset/metadata.csv')
+
+async function prepareDataset() {
+    const metadata = await fetch('./dataset/metadata.csv')
         .then(response => response.text())
         .then(data => Papa.parse(data, {header: true}))
         .then(result => result.data);
 
-    let filenames = metadata.map(item => `./assets/dataset/Images/${item.filename}`);
+    let filenames = metadata.map(item => `${IMG_DIR}${item.filename}`);
     let labels = metadata.map(item => parseInt(item.label)); // Assuming labels are integers (0 or 1)
     tf.util.shuffleCombo(filenames, labels);
 
     // Load and preprocess images
-    const imageFeatures = await Promise.all(filenames.map(loadAndPreprocessImage));
+    const imageFeatures = await Promise.all(filenames.map(async (filename) => {
+        // Load and preprocess the image
+        const preprocessedImage = await loadAndPreprocessImage(filename);
+        // Make predictions using mobileNetBase and apply squeeze
+        return mobileNetBase.predict(preprocessedImage).squeeze();
+    }));
     console.log("Data preparation complete!")
 
     return [imageFeatures, labels];
+}
+
+function logProgress(epoch, logs) {
+    console.log('Data for epoch ' + epoch, logs);
 }
 
 async function trainModel() {
@@ -144,22 +109,94 @@ async function saveModel() {
     combinedModel.add(mobileNetBase);
     combinedModel.add(model);
 
-    combinedModel.compile({
+    await combinedModel.compile({
         optimizer: 'adam',
         loss: 'binaryCrossentropy'
     });
-    combinedModel.save('downloads://my-model')
+    await combinedModel.save('downloads://my-model')
 
     console.log('Model saved!')
 }
 
-// Start training
+async function loadModel() {
+    // Load the model architecture and weights
+    combinedModel = await tf.loadLayersModel('./model/my-model.json');
+    combinedModel.summary()
 
-loadMobileNetFeatureModel()
-    .then(() => trainModel())
-    .then(() => saveModel());
-
-
-function logProgress(epoch, logs) {
-    console.log('Data for epoch ' + epoch, logs);
+    console.log('Model loaded!');
 }
+
+async function makePrediction(filenames) {
+    let preprocessedDataArray = await Promise.all(filenames.map(async (filename) => {
+        return await loadAndPreprocessImage(filename);
+    }));
+    preprocessedDataArray = tf.stack(preprocessedDataArray,0).squeeze(1)
+    let prediction = await combinedModel.predict(preprocessedDataArray);
+    prediction = prediction.dataSync()
+    console.log("filenames", filenames);
+    console.log("raw score", prediction);
+    prediction = prediction.map(value => value > 0.5 ? 1 : 0);
+    console.log("prediction", prediction);
+
+    return prediction;
+}
+
+
+//// Start training
+// loadMobileNetFeatureModel()
+//     .then(() => createNewModelHead())
+//     .then(() => trainModel())
+//     .then(() => saveModel())
+
+
+// ------------------------------
+// HTML Events
+// ------------------------------
+const IMG_DIR = "./dataset/Images/"
+const TRASH_RED_ICON = "./assets/icons/trash_red.png"
+const TRASH_GREEN_ICON = "./assets/icons/trash_green.png"
+const ROADS = [
+    {name: "Clean Road", image: IMG_DIR + "clean_1.jpg",},
+    {name: "Clean Road", image: IMG_DIR + "clean_2.jpg",},
+    {name: "Clean Road", image: IMG_DIR + "clean_3.jpg",},
+    {name: "Clean Road", image: IMG_DIR + "clean_4.jpg",},
+    {name: "Clean Road", image: IMG_DIR + "clean_5.jpg",},
+    {name: "Dirty Road", image: IMG_DIR + "dirty_1.jpg",},
+    {name: "Dirty Road", image: IMG_DIR + "dirty_2.jpg",},
+    {name: "Dirty Road", image: IMG_DIR + "dirty_3.jpg",},
+    {name: "Dirty Road", image: IMG_DIR + "dirty_4.jpg",},
+    {name: "Dirty Road", image: IMG_DIR + "dirty_5.jpg",},
+    // Add more ROADS as needed
+];
+const ALL_IMAGES = ROADS.map(road => road.image)
+const ALL_IMAGES_FLATTENED = [].concat(ALL_IMAGES)
+
+async function updateMarkers(markers, dirtinessClassifications){
+    markers.forEach((marker, index) => {
+        let road = ROADS[index];
+        let prediction = dirtinessClassifications[index];
+
+        // Set background image based on prediction value
+        marker.style.backgroundImage = `url(${prediction === 1 ? TRASH_RED_ICON : TRASH_GREEN_ICON})`;
+
+        marker.addEventListener('click', () => {
+            // Update sidebar content
+            document.getElementById('road-name').innerText = road.name;
+            document.getElementById('road-image').src = road.image;
+            // Show sidebar
+            document.getElementById('sidebar').style.display = 'block';
+        });
+    });
+}
+
+// Add click event listener to each marker
+const markers = document.querySelectorAll('.marker');
+loadModel()
+    .then(() => makePrediction(ALL_IMAGES_FLATTENED))
+    .then(predictions => updateMarkers(markers, predictions));
+// Close sidebar when clicking outside of it
+document.addEventListener('click', (event) => {
+    if (!event.target.closest('#sidebar') && !event.target.classList.contains('marker')) {
+        document.getElementById('sidebar').style.display = 'none';
+    }
+});
